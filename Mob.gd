22ef0f35fd2c35e09a9c87724d7f08bce6770459
Snipexkillo <akshah1201@gmail.com>
2023-04-08 15:02:40 -0500

    Update Mob.gd

--- Mob.gd
+++ Mob.gd
@@ -2,5 +2,5 @@ extends RigidBody2D
 func _ready():
 	$AnimatedSprite.playing = true
 	$AnimatedSprite.animation = "move"
-	func _on_VisibilityNotifier2D_screen_exited():
+func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
